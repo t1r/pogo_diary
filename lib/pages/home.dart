165325cfd8f6220d0321c81295f4d47d8106d401
Akeksandr Ermolenko <AlexanderErmolenko@yandex.ru>
2020-01-11 13:58:38 +0300

    feature/Types add buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,24 +6,41 @@ import 'package:pogo_diary/data/pokemon_type_value.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final PokemonTypesBloc typesBloc = BlocProvider.of<PokemonTypesBloc>(context);
+    final PokemonTypesBloc typesBloc =
+        BlocProvider.of<PokemonTypesBloc>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('AppBar'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StreamBuilder<List<PokemonTypes>>(
-                stream: typesBloc.outTypes,
-                initialData: [],
-                builder: (BuildContext context, AsyncSnapshot<List<PokemonTypes>> snapshot) {
-                  return Text('Types: ${snapshot.data}');
-                }),
-          ],
-        ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: StreamBuilder<List<PokemonTypes>>(
+            stream: typesBloc.outTypes,
+            initialData: [],
+            builder: (BuildContext context,
+                AsyncSnapshot<List<PokemonTypes>> snapshot) {
+              return GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                padding: const EdgeInsets.all(4.0),
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                children: snapshot.data.map((f) {
+                  return RaisedButton(
+                    onPressed: null,
+                    color: const Color(0xFF5ca753),
+                    disabledColor: const Color(0xFF5ca753),
+                    textColor: const Color(0xFFFFFFFF),
+                    disabledTextColor: const Color(0xFFFFFFFF),
+                    child: Text(
+                      '$f',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  );
+                }).toList(),
+              );
+            }),
       ),
     );
   }
